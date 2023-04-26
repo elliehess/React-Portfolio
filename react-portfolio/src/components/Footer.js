0170ed7cafa1003d352f0023a17c8df4a5286364
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-04-25 22:08:50 -0700

    footer links added

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
-// import LinkedInIcon from '@material-ui/icons';
-// import InstagramIcon from '@material-ui/icons';
+import {FaGithub} from 'react-icons/fa'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+
+
 import '../styles/Footer.css';
 
 function Footer(){
     return (
         <div className='footer'>
             <div className='socialMedia'>
-                {/* <InstragramIcon />
-                <LinkedInIcon /> */}
+            <a href="https://github.com/elliehess" target="_blank"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/ellie-hess-56527061/" target="_blank"><FaLinkedin /></a>
+                <FaTwitter />
+                {/* i do not have a twitter or stacked overflow profile */}
             </div>
             <p> &copy; 2023 github.com/elliehess</p>
         </div>
