778389b9984163f93206cdc4bca67d745122db16
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-05-23 22:22:57 -0700

    first link

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,35 +1,29 @@
-import React, { useState } from 'react';
-import { FaGithub } from 'react-icons/fa';
-import { FaAppStore } from 'react-icons/fa';
+import React from 'react';
+import { FaGithub, FaAppStore } from 'react-icons/fa';
 
-
-//Use window.open instead of useNavigate to open a new window with the github repo while the React portfolio is still running in the background
-// i need to introduce another element websiteUrl to open the website in a new tab
 function Project({ image, name, githubUrl, websiteUrl }) {
   const openGithub = () => {
-    window.open(githubUrl, '_blank');};
-  const [showGithub, setShowGithub] = useState(false);
+    window.open(githubUrl, '_blank');
+  };
 
-  //if mouse hovers over project item, show the github icon
   return (
-    <div
-      className="projectItem"
-      onMouseEnter={() => setShowGithub(true)}
-      onMouseLeave={() => setShowGithub(false)}
-      onClick={openGithub}
-    >
+    <div className="projectItem" onClick={openGithub}>
       <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-      <h1>{name}
-      <div className='appIcon'><a href={websiteUrl} target='blank'><FaAppStore size={32} /></a></div>
-      </h1>
-      {showGithub && (
-        <a href={githubUrl}target="_blank" rel="noopener noreferrer" className="githubIcon">
+      <h1>{name}</h1>
+      <div className='appIcon'>
+        <a href={websiteUrl} target='blank'>
+          <FaAppStore size={32} />
+        </a>
+      </div>
+      <div className="githubIcon">
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
           <FaGithub size={32} />
         </a>
-      )}
+      </div>
     </div>
   );
 }
 
 export default Project;
+
 
